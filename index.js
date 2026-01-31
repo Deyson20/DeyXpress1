9167ca121d918c5b1d200f338030cad6b1f2abed
Deyson20 <137962390+Deyson20@users.noreply.github.com>
2026-01-31 07:15:56 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import html from './index.html';
+
 export default {
   async fetch(request, env, ctx) {
-    return new Response("Hola mundo desde el Worker");
+    return new Response(html, {
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    });
   },
 };
